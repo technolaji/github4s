5921aaa3f89a0b38125338126822888a41886115
Rafa Paradela <rafa.p@47deg.com>
2016-06-06 13:03:11 +0200

    Typed implicit value

--- src/test/scala/github4s/utils/DummyGithubUrls.scala
+++ src/test/scala/github4s/utils/DummyGithubUrls.scala
@@ -4,7 +4,7 @@ import github4s.GithubApiUrls
 
 trait DummyGithubUrls {
 
-  implicit val dummyUrls = GithubApiUrls(
+  implicit val dummyUrls: GithubApiUrls = GithubApiUrls(
     baseUrl        = "http://127.0.0.1:9999/",
     authorizeUrl   = "http://127.0.0.1:9999/authorize?client_id=%s&redirect_uri=%s&scope=%s&state=%s",
     accessTokenUrl = "http://127.0.0.1:9999/login/oauth/access_token"
