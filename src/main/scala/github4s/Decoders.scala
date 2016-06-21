f1e9d88db59097d8c6694a8eb5021473cd6f12bb
Rafa Paradela <rafa.p@47deg.com>
2016-06-21 12:19:06 +0200

    Enable commented parameters

--- src/main/scala/github4s/Decoders.scala
+++ src/main/scala/github4s/Decoders.scala
@@ -53,6 +53,42 @@ object Decoders {
       ssh_url ← c.downField("ssh_url").as[String]
       clone_url ← c.downField("clone_url").as[String]
       svn_url ← c.downField("svn_url").as[String]
+      forks_url ← c.downField("forks_url").as[Option[String]]
+      keys_url ← c.downField("keys_url").as[Option[String]]
+      collaborators_url ← c.downField("collaborators_url").as[Option[String]]
+      teams_url ← c.downField("teams_url").as[Option[String]]
+      hooks_url ← c.downField("hooks_url").as[Option[String]]
+      issue_events_url ← c.downField("issue_events_url").as[Option[String]]
+      events_url ← c.downField("events_url").as[Option[String]]
+      assignees_url ← c.downField("assignees_url").as[Option[String]]
+      branches_url ← c.downField("branches_url").as[Option[String]]
+      tags_url ← c.downField("tags_url").as[Option[String]]
+      blobs_url ← c.downField("blobs_url").as[Option[String]]
+      git_tags_url ← c.downField("git_tags_url").as[Option[String]]
+      git_refs_url ← c.downField("git_refs_url").as[Option[String]]
+      trees_url ← c.downField("trees_url").as[Option[String]]
+      statuses_url ← c.downField("statuses_url").as[Option[String]]
+      languages_url ← c.downField("languages_url").as[Option[String]]
+      stargazers_url ← c.downField("stargazers_url").as[Option[String]]
+      contributors_url ← c.downField("contributors_url").as[Option[String]]
+      subscribers_url ← c.downField("subscribers_url").as[Option[String]]
+      subscription_url ← c.downField("subscription_url").as[Option[String]]
+      commits_url ← c.downField("commits_url").as[Option[String]]
+      git_commits_url ← c.downField("git_commits_url").as[Option[String]]
+      comments_url ← c.downField("comments_url").as[Option[String]]
+      issue_comment_url ← c.downField("issue_comment_url").as[Option[String]]
+      contents_url ← c.downField("contents_url").as[Option[String]]
+      compare_url ← c.downField("compare_url").as[Option[String]]
+      merges_url ← c.downField("merges_url").as[Option[String]]
+      archive_url ← c.downField("archive_url").as[Option[String]]
+      downloads_url ← c.downField("downloads_url").as[Option[String]]
+      issues_url ← c.downField("issues_url").as[Option[String]]
+      pulls_url ← c.downField("pulls_url").as[Option[String]]
+      milestones_url ← c.downField("milestones_url").as[Option[String]]
+      notifications_url ← c.downField("notifications_url").as[Option[String]]
+      labels_url ← c.downField("labels_url").as[Option[String]]
+      releases_url ← c.downField("releases_url").as[Option[String]]
+      deployments_url ← c.downField("deployments_url").as[Option[String]]
     } yield Repository(
       id           = id,
       name         = name,
@@ -83,12 +119,48 @@ object Decoders {
         has_pages         = has_pages
       ),
       urls         = RepoUrls(
-        url       = url,
-        html_url  = html_url,
-        git_url   = git_url,
-        ssh_url   = ssh_url,
-        clone_url = clone_url,
-        svn_url   = svn_url
+        url               = url,
+        html_url          = html_url,
+        git_url           = git_url,
+        ssh_url           = ssh_url,
+        clone_url         = clone_url,
+        svn_url           = svn_url,
+        forks_url         = forks_url,
+        keys_url          = keys_url,
+        collaborators_url = collaborators_url,
+        teams_url         = teams_url,
+        hooks_url         = hooks_url,
+        issue_events_url  = issue_events_url,
+        events_url        = events_url,
+        assignees_url     = assignees_url,
+        branches_url      = branches_url,
+        tags_url          = tags_url,
+        blobs_url         = blobs_url,
+        git_tags_url      = git_tags_url,
+        git_refs_url      = git_refs_url,
+        trees_url         = trees_url,
+        statuses_url      = statuses_url,
+        languages_url     = languages_url,
+        stargazers_url    = stargazers_url,
+        contributors_url  = contributors_url,
+        subscribers_url   = subscribers_url,
+        subscription_url  = subscription_url,
+        commits_url       = commits_url,
+        git_commits_url   = git_commits_url,
+        comments_url      = comments_url,
+        issue_comment_url = issue_comment_url,
+        contents_url      = contents_url,
+        compare_url       = compare_url,
+        merges_url        = merges_url,
+        archive_url       = archive_url,
+        downloads_url     = downloads_url,
+        issues_url        = issues_url,
+        pulls_url         = pulls_url,
+        milestones_url    = milestones_url,
+        notifications_url = notifications_url,
+        labels_url        = labels_url,
+        releases_url      = releases_url,
+        deployments_url   = deployments_url
       )
     )
   }
