437741da30bd8fa63c244ea33076a8558389843c
Juan Pedro Moreno <juanpmorest@gmail.com>
2016-10-12 13:24:45 +0200

    Implements tailRecM for MonadError at scalaz module

--- scalaz/src/main/scala/github4s/scalaz/Implicits.scala
+++ scalaz/src/main/scala/github4s/scalaz/Implicits.scala
@@ -1,17 +1,19 @@
 package github4s.scalaz
 
-import cats.MonadError
+import cats.{ MonadError, FlatMap }
 import scalaz.concurrent.Task
 
 object implicits {
 
-  implicit val g4sTaskMonadError: MonadError[Task, Throwable] = new MonadError[Task, Throwable] {
+  implicit def g4sTaskMonadError(implicit FM: FlatMap[Task]): MonadError[Task, Throwable] = new MonadError[Task, Throwable] {
 
     override def pure[A](x: A): Task[A] = Task.now(x)
 
     override def map[A, B](fa: Task[A])(f: A ⇒ B): Task[B] = fa.map(f)
 
     override def flatMap[A, B](fa: Task[A])(ff: A ⇒ Task[B]): Task[B] = fa.flatMap(ff)
+
+    override def tailRecM[A, B](a: A)(f: A ⇒ Task[Either[A, B]]): Task[B] = FM.tailRecM(a)(f)
 
     override def raiseError[A](e: Throwable): Task[A] = Task.fail(e)
 
