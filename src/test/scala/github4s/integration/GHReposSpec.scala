3d82dcffd8d4d8c3c80ececb4eeb2f66d2702b43
Juan Pedro Moreno <juanpmorest@gmail.com>
2016-10-24 19:34:21 +0200

    Adds auto header

--- src/test/scala/github4s/integration/GHReposSpec.scala
+++ src/test/scala/github4s/integration/GHReposSpec.scala
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2016 47 Degrees, LLC. <http://www.47deg.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package github4s.integration
 
 import cats.Id
@@ -7,7 +28,7 @@ import github4s.GithubResponses._
 import github4s.implicits._
 import github4s.Github
 import github4s.utils.TestUtils
-import org.scalatest.{ Matchers, FlatSpec }
+import org.scalatest.{Matchers, FlatSpec}
 
 class GHReposSpec extends FlatSpec with Matchers with TestUtils {
 
@@ -42,7 +63,8 @@ class GHReposSpec extends FlatSpec with Matchers with TestUtils {
   }
 
   "Repos >> ListContributors" should "return the expected list of contributors for valid data" in {
-    val response = Github(accessToken).repos.listContributors(validRepoOwner, validRepoName).exec[Id]
+    val response =
+      Github(accessToken).repos.listContributors(validRepoOwner, validRepoName).exec[Id]
     response should be('right)
 
     response.toOption map { r ⇒
@@ -53,7 +75,8 @@ class GHReposSpec extends FlatSpec with Matchers with TestUtils {
   }
 
   it should "return error for invalid repo name" in {
-    val response = Github(accessToken).repos.listContributors(invalidRepoName, validRepoName).exec[Id]
+    val response =
+      Github(accessToken).repos.listContributors(invalidRepoName, validRepoName).exec[Id]
     response should be('left)
   }
 
--- src/test/scala/github4s/utils/TestUtils.scala
+++ src/test/scala/github4s/utils/TestUtils.scala
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2016 47 Degrees, LLC. <http://www.47deg.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package github4s.utils
 
 import github4s.free.domain.GistFile
@@ -9,41 +30,41 @@ trait TestUtils {
   val accessToken = sys.props.get("token")
   def tokenHeader = "token " + accessToken.getOrElse("")
 
-  val validUsername = "rafaparadela"
+  val validUsername   = "rafaparadela"
   val invalidUsername = "GHInvalidaUserName"
   val invalidPassword = "invalidPassword"
 
-  def validBasicAuth = s"Basic ${base64(s"$validUsername:")}"
+  def validBasicAuth   = s"Basic ${base64(s"$validUsername:")}"
   def invalidBasicAuth = s"Basic ${base64(s"$validUsername:$invalidPassword")}"
 
-  val validScopes = List("public_repo")
-  val validNote = "New access token"
-  val validClientId = "e8e39175648c9db8c280"
+  val validScopes         = List("public_repo")
+  val validNote           = "New access token"
+  val validClientId       = "e8e39175648c9db8c280"
   val invalidClientSecret = "1234567890"
-  val validCode = "code"
-  val invalidCode = "invalid-code"
+  val validCode           = "code"
+  val invalidCode         = "invalid-code"
 
-  val validRepoOwner = "47deg"
-  val validRepoName = "github4s"
-  val invalidRepoName = "GHInvalidRepoName"
+  val validRepoOwner   = "47deg"
+  val validRepoName    = "github4s"
+  val invalidRepoName  = "GHInvalidRepoName"
   val validRedirectUri = "http://localhost:9000/_oauth-callback"
-  val validPage = 1
-  val invalidPage = 999
-  val validPerPage = 100
+  val validPage        = 1
+  val invalidPage      = 999
+  val validPerPage     = 100
 
-  val validSinceInt = 100
+  val validSinceInt   = 100
   val invalidSinceInt = 999999999
 
-  val okStatusCode = 200
-  val createdStatusCode = 201
+  val okStatusCode           = 200
+  val createdStatusCode      = 201
   val unauthorizedStatusCode = 401
-  val notFoundStatusCode = 404
+  val notFoundStatusCode     = 404
 
-  val validAnonParameter = "true"
+  val validAnonParameter   = "true"
   val invalidAnonParameter = "X"
 
   val validGistDescription = "A Gist"
-  val validGistPublic = true
-  val validGistFile = GistFile("val meaningOfLife = 42")
-  val validGistFiles = Map("file1.scala" → validGistFile)
+  val validGistPublic      = true
+  val validGistFile        = GistFile("val meaningOfLife = 42")
+  val validGistFiles       = Map("file1.scala" → validGistFile)
 }
