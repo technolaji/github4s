8c5c50689b68c16bac621c216675557268bf4b18
Juan Pedro Moreno <juanpmorest@gmail.com>
2017-03-23 17:03:42 +0100

    Excludes BuildInfo class from packaging (#65)
    * Excludes BuildInfo class from packaging
    * Disables non-error logging in tests
    * Adds explanatory comment

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -70,9 +70,9 @@ object ProjectPlugin extends AutoPlugin {
         organizationName = "47 Degrees",
         groupId = "com.47deg",
         organizationHomePage = url("http://47deg.com"),
-        organizationEmail = "hello@47deg.com",
-        license = sbtorgpolicies.model.MITLicense
+        organizationEmail = "hello@47deg.com"
       ),
+      orgLicense := sbtorgpolicies.model.MITLicense,
       scalaVersion := scalac.`2.12`,
       crossScalaVersions := "2.10.6" :: scalac.crossScalaVersions,
       scalaOrganization := "org.scala-lang",
@@ -82,6 +82,14 @@ object ProjectPlugin extends AutoPlugin {
       }),
       headers := Map(
         "scala" -> MIT("2016", "47 Degrees, LLC. <http://www.47deg.com>")
-      )
+      ),
+      // This is necessary to prevent packaging the BuildInfo with
+      // sensible information like the Github token. Do not remove.
+      mappings in (Compile, packageBin) ~= { (ms: Seq[(File, String)]) =>
+        ms filter {
+          case (_, toPath) =>
+            !toPath.startsWith("github4s/BuildInfo")
+        }
+      }
     ) ++ shellPromptSettings
 }
