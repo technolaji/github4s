a9826fec0641fd95a59442f8e5bab39dec10da73
Mobolaji Abiodun <mabiodun@mdsol.com>
2018-11-29 15:58:02 +0000

    added webhook op to app

--- github4s/shared/src/main/scala/github4s/app.scala
+++ github4s/shared/src/main/scala/github4s/app.scala
@@ -27,5 +27,7 @@ object app {
   type COGH05[A]   = EitherK[GitDataOp, COGH04, A]
   type COGH06[A]   = EitherK[PullRequestOp, COGH05, A]
   type COGH07[A]   = EitherK[ActivityOp, COGH06, A]
-  type GitHub4s[A] = EitherK[OrganizationOp, COGH07, A]
+  type COGH08[A]   = EitherK[WebhookOp, COGH07, A]
+  type GitHub4s[A] = EitherK[OrganizationOp, COGH08, A]
+
 }
